4673d00c398c9d92063e8c3d03f22e2b54acab1a
roychen37326 <1206664273@qq.com>
2023-03-26 19:13:20 +0800

    second commit

--- fa21-proj2-starter/src/dot.s
+++ fa21-proj2-starter/src/dot.s
@@ -20,11 +20,39 @@
 dot:
 
     # Prologue
-
+    addi t0, x0, 1
+    blt a2, t0, exit_element
+    blt a3, t0, exit_stride
+    blt a4, t0, exit_stride
 
 loop_start:
+    addi t1, x0, 0 # rep arr1 index
+    addi t2, x0, 0 # rep arr2 index
+    addi sp, sp, -4
+    sw ra, 0(sp)
 
+    addi t3, x0, 0 # rep ans
+    addi t4, x0, 0 # rep arr1 len to add
+    addi t5, x0, 0 # rep arr2 len to add
 
+loop_continue:
+    bge t1, a2, loop_end
+    bge t2, a2, loop_end
+
+    slli t4, 
+
+loop_end:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    
+
+exit_element:
+    li a1, 57
+    call exit2
+
+exit_stride:
+    li a1, 58
+    call exit2
 
 
 
